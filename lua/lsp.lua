06e260f0a0d081992b2efcfe953e09e01bf8dc06
Игор Вујчић <igorv@Igors-MacBook-Pro.local>
2026-07-31 20:27:21 +0200

    Rust LSP

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -30,3 +30,10 @@ vim.diagnostic.config({
   underline = false,
 })
 
+vim.lsp.config['rust_analyzer'] = {
+  cmd = { 'rust-analyzer' },
+  filetypes = { 'rust' },
+  root_markers = { 'Cargo.toml', '.git' },
+}
+vim.lsp.enable('rust_analyzer')
+
